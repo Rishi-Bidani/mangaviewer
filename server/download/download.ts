e7c08310cba5ab12c9f7cd51d1cf2b84d97a06e7
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2022-11-08 17:07:31 +1100

    separated out code into different files

--- server/download/download.ts
+++ server/download/download.ts
@@ -2,9 +2,9 @@ import { JSDOM } from "jsdom";
 import { promises as fs } from "fs";
 import path from "path";
 import fetch from "node-fetch";
-import { Response } from "node-fetch";
 
 import { MangaClash } from "./classnames.js";
+import { MetaData, IMetaDataChapter } from "./logwriter.js";
 import { ImageFetchError, DownloadImageError, MainFetchError } from "./downloaderrors.js";
 
 interface IChapterLink {
@@ -12,52 +12,7 @@ interface IChapterLink {
     link: URL;
 }
 
-interface IMetaDataRoot {
-    mangaName: string | undefined | null;
-    mangaLink: string | undefined | null;
-    availableChapters: number;
-    downloadedChapters: number;
-}
-
-interface IMetaDataChapter {
-    chapterName: string | undefined | null;
-    chapterLink: URL | undefined | null;
-    images: (string | undefined)[];
-    numberOfImages: number;
-}
-
 const BASE_MANGA_FOLDER = path.resolve("test");
-
-class MetaData {
-    // Create a log file for the root folder
-    static async root(rootDom: JSDOM): Promise<void> {
-        // The file will follow IMetaDataRoot structure
-        const mangaName = rootDom.window.document.querySelector(MangaClash.title)?.textContent;
-        const mangaLink = rootDom.window.document
-            .querySelector(MangaClash.selfLink)
-            ?.getAttribute("href");
-        const availableChapters = rootDom.window.document.querySelectorAll(
-            MangaClash.chapterList
-        ).length;
-        const downloadedChapters = 0;
-        const metaData: IMetaDataRoot = {
-            mangaName: mangaName?.trim(),
-            mangaLink: mangaLink,
-            availableChapters: availableChapters,
-            downloadedChapters: downloadedChapters,
-        };
-        const metaDataPath = path.resolve(BASE_MANGA_FOLDER, "meta.json");
-        await fs.writeFile(metaDataPath, JSON.stringify(metaData));
-    }
-
-    // Create a log file for the chapter folder
-    static async chapter(logObject: IMetaDataChapter, chapterFolder: string): Promise<void> {
-        await fs.writeFile(
-            path.join(chapterFolder, "log.json"),
-            JSON.stringify(logObject, null, 4)
-        );
-    }
-}
 
 async function getDom(link: URL): Promise<JSDOM> {
     try {
