0e92129c87cb06f8192904d9043804d629fab0f6
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2022-11-24 22:10:24 +0530

    working on ui for displaying manga images

--- client/src/components/read/SideBarPC.vue
+++ client/src/components/read/SideBarPC.vue
@@ -4,8 +4,16 @@
             {{ mangaName }}
         </header>
         <div class="container__chapter-list">
-            <ol class="flex-column">
-                <li v-for="chapter in chapterList" :key="chapter" class="item__chapter-list">
+            <ol class="flex-column" ref="chapterOrderedList">
+                <li
+                    ref="chapterListItem"
+                    v-for="(chapter, index) in chapterList"
+                    :key="chapter"
+                    class="item__chapter-list"
+                    :class="index === activeElementIndex ? 'active' : ''"
+                    :data-chapter-name="chapter"
+                    @click="loadChapter"
+                >
                     {{ chapter }}
                 </li>
             </ol>
@@ -14,7 +22,7 @@
 </template>
 
 <script lang="ts" setup>
-import { defineProps, inject, onMounted, ref } from "vue";
+import { defineProps, ref } from "vue";
 
 // types
 import { Ref } from "vue";
@@ -23,6 +31,49 @@ const props = defineProps<{
     mangaName: string;
     chapterList: any;
 }>();
+
+const activeElementIndex = ref(0);
+const chapterListItem: Ref<any> = ref([]);
+
+const Chapters = {
+    clearActive: () => {
+        chapterListItem.value.forEach((element: HTMLElement) => {
+            element.classList.remove("active");
+        });
+    },
+
+    setActive: (chapterName: string) => {
+        chapterListItem.value.forEach((element: HTMLElement) => {
+            if (element.dataset.chapterName === chapterName) {
+                element.classList.add("active");
+            }
+        });
+    },
+};
+
+// const chapterOrderedList: Ref<any> = ref();
+// // const firstChapterName = computed(() => {
+// //     return props.chapterList.value[0];
+// // });
+
+// onMounted(async () => {
+//     const defaultActive = 1;
+//     const listElement = chapterOrderedList.value.querySelector(`:first-child`);
+//     listElement.classList.add("active");
+// });
+
+function loadChapter(event: Event) {
+    const listElement = event.target as HTMLElement;
+    const chapterName = listElement.dataset.chapterName as string;
+
+    //
+
+    // set class active on list element
+    Chapters.clearActive();
+    listElement.classList.add("active");
+    // activeListElement.value.classList.remove("active");
+    // activeListElement.value = listElement;
+}
 </script>
 
 <style scoped>
@@ -48,5 +99,10 @@ ol {
     padding-block: 1rem;
     padding-inline: 0.5rem;
     cursor: pointer;
+}
+
+.active {
+    background-color: var(--sidebar-active-clr);
+    color: var(--primary-text-clr);
 }
 </style>
